79cf57a4edf7e3abbe647d1e568af5aeb5a2deb0
Marco Aurélio Om <mmaureliom@gmail.com>
2019-06-08 19:20:30 -0300

    Delete IResponder.java

--- src/zombieHealth/interfaces/IResponder.java
+++
@@ -1,6 +0,0 @@
-package zombieHealth.interfaces;
-//Interface de referência: não deve ser alterada para Parte 1 do trabalho.
-public interface IResponder {
-	public String ask(String question);
-    public boolean finalAnswer(String answer);
-}
