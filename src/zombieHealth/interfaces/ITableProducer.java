bc5205b845569587e862031927d6ca04872c4e34
Marco Aurélio Om <mmaureliom@gmail.com>
2019-06-08 19:20:38 -0300

    Delete ITableProducer.java

--- src/zombieHealth/interfaces/ITableProducer.java
+++
@@ -1,8 +0,0 @@
-package zombieHealth.interfaces;
-import weka.core.Instances;
-
-public interface ITableProducer {
-	String[] requestAttributes();
-	String[][] requestInstances();
-	Instances requestInstanciasArvore();
-}
